193e513259c453e1bcaa966377f65b0efabddbf1
Antonio <francesca@Francescas-MacBook-Air.local>
2024-10-02 12:08:02 +0200

    add subplot_visualizer

---
+++ plot/plot_compare_models.py
@@ -0,0 +1,23 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from plot_average_energy import plot_average_energy
+from plot_environment_health import plot_environment_health
+from plot_agent_behavior import plot_agent_behavior
+from plot_energy_vs_health import plot_energy_vs_health
+from plot_tokens import plot_tokens
+
+def compare_models(models):
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(12, 8))
+    for index, model in enumerate(models):
+        print(f"Index: {index}, Element: {model}")
+    # for model in models:
+        # plt.subplot(2, 2, 1)
+        # plot_tokens(plt, model)
+        # plt.subplot(2, 2, 2)
+        # plot_average_energy(plt, model)
+        # plt.tight_layout()
+        # plt.show()
+    
--- write_plot.py
+++ write_plot.py
@@ -1,8 +1,5 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'plot')))
-import plot
-
 
 def get_fixed_model():
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixed-incentives-model')))
@@ -16,6 +13,20 @@ def get_variable_model():
     model = PopulationModel(10, 10, 50, 365)
     return model
 
-if __name__ == "__main__":
+def compare_models():
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'plot')))
+    import plot
+    fixed_model = get_fixed_model()
+    variable_model = get_variable_model()
+    models = [fixed_model, variable_model]
     #plot.run_all(get_fixed_model())
-    plot.run_all(get_variable_model())
+    plot.run_all(models)
+
+def subplot_visualizer():
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'plot')))
+    from plot.subplot_visualizer import subplot_visualizer
+    subplot_visualizer(get_fixed_model())
+
+if __name__ == "__main__":
+    subplot_visualizer()
+
