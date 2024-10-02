dea0f6a9a30e9a90730da17626531d6fa7d24bed
Antonio <francesca@Francescas-MacBook-Air.local>
2024-10-02 21:48:37 +0200

    fixed ModuleNotFoundError

--- write_tex.py
+++ write_tex.py
@@ -5,15 +5,15 @@ import texs
 
 
 def get_fixed_model():
-    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixed-incentives-model')))
-    from population_model import PopulationModel
-    model = PopulationModel(10, 10, 50, 365)
+    sys.path.insert(0, variable_path) if (variable_path := os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixed-incentives-model'))) not in sys.path else None
+    import fixed_population_model
+    model = fixed_population_model.PopulationModel(10, 10, 50, 350)
     return model
 
 def get_variable_model():
-    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'variable-incentives-model')))
-    from population_model import PopulationModel
-    model = PopulationModel(10, 10, 50, 365)
+    sys.path.insert(0, variable_path) if (variable_path := os.path.abspath(os.path.join(os.path.dirname(__file__), 'variable-incentives-model'))) not in sys.path else None
+    import variable_population_model
+    model = variable_population_model.PopulationModel(10, 10, 50, 350)
     return model
 
 if __name__ == "__main__":
