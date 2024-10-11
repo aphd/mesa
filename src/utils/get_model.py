90504a54ef5e40c30d079aa9b9460a48d3f716ef
Antonio <francesca@Francescas-MacBook-Air.local>
2024-10-11 15:22:33 +0200

    add os.getenv(FN_DIR)

--- src/utils/get_model.py
+++ src/utils/get_model.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-width, height, N, duration = 10, 10, 50, 500
+width, height, N, duration = 10, 10, 50, 356
 
 def get_fixed_model():
     sys.path.insert(0, variable_path) if (variable_path := os.path.abspath(os.path.join(os.path.dirname(__file__), '../../fixed-incentives-model'))) not in sys.path else None
